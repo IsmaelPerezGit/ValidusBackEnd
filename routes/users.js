9b757ace274947eff00bef6848a6b686cfb84002
Ismael Perez <ismaelperezemail@gmail.com>
2017-11-22 21:06:13 -0700

    added post new user route

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,14 @@ var knex = require('../db/knex')
 //Splash page
 router.get('/', function(req, res, next) {
     knex.raw(`select * from users`)
-        .then(function(users) {
-          res.send(users.rows);
+        .then(function(user) {
+          res.send(user.rows[0]);
         })
+});
+
+//Post new user to database
+router.post('/new', function(req, res, next) {
+    knex.raw(`insert into users (email, username, user_token, team_id) values ('${req.body.email}', '${req.body.username}', '${req.body.user_token}', '${req.body.team_id}')`)
 });
 
 

bb7bcfffa01037e3cd4c3fc9f46a714a557ad070
Ismael Perez <ismaelperezemail@gmail.com>
2017-11-23 13:25:32 -0700

    reformatted user routes

--- routes/users.js
+++ routes/users.js
@@ -4,21 +4,23 @@ var knex = require('../db/knex')
 
 
 //Splash page
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     knex.raw(`select * from users`)
-        .then(function(user) {
-          res.send(user.rows[0]);
+        .then(function (user) {
+            res.send(user.rows[0]);
         })
 });
 
 //Post new user to database
-router.post('/new', function(req, res, next) {
+router.post('/new', function (req, res, next) {
     knex.raw(`insert into users (email, username, user_token) values (
     '${req.body.email}', 
     '${req.body.username}', 
     '${req.body.user_token}'
     )`)
-        .then(() => {res.send('success')} )
+        .then(() => {
+            res.send('success')
+        })
 });
 
 

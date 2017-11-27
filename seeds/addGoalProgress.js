577906468f2a9774b36d672f354fd01088cadd0a
Ismael Perez <ismaelperezemail@gmail.com>
2017-11-27 13:04:52 -0700

    minor

--- seeds/addGoalProgress.js
+++ seeds/addGoalProgress.js
@@ -1,10 +1,10 @@
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
     // Deletes ALL existing entries
     return knex('goal_progress').del()
         .then(function () {
             // Inserts seed entries
             return knex('goal_progress').insert([
-                {user_id:1 , goal_id:1, counter:0 }
+                {user_id: 1, goal_id: 1, counter: 0}
             ]);
         });
 };

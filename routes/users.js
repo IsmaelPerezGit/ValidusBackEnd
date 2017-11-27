6ce545408573b97fd1339e631d10cb8b1333b045
Ismael Perez <ismaelperezemail@gmail.com>
2017-11-27 10:58:19 -0700

    fixed bug

--- routes/users.js
+++ routes/users.js
@@ -24,9 +24,9 @@ router.post('/new', function (req, res, next) {
 });
 
 //Show single user
-router.get('/:id', function (req, res, next) {
+router.get('/:user_token', function (req, res, next) {
     //var userID = req.params.id;
-    knex.raw(`select * from users where id = '${req.params.id}'`)
+    knex.raw(`select * from users where user_token = '${req.params.user_token}'`)
         .then(function (user) {
             res.send(user.rows[0])
         })

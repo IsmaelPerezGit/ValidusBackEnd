698b38babf7e729a02aa3accdaaa6d8b31da83e0
Ismael Perez <ismaelperezemail@gmail.com>
2017-11-28 17:55:00 -0700

    added route for adding goal

--- routes/goals.js
+++ routes/goals.js
@@ -11,4 +11,22 @@ router.get('/', function (req, res, next) {
         })
 });
 
+router.post('/new', function (req, res, next) {
+    knex.raw(`insert into goals (target, start_date, weeks, Sun, Mon, Tues, Wed, Thurs, Fri, Sat) values (
+    '${req.body.target}', 
+    '${req.body.start_date}', 
+    '${req.body.weeks}'
+    '${req.body.Sun}', 
+    '${req.body.Mon}', 
+    '${req.body.Tues}'
+    '${req.body.Wed}', 
+    '${req.body.Thurs}', 
+    '${req.body.Fri}'
+    '${req.body.Sat}'
+    )`)
+        .then(() => {
+            res.send('success')
+        })
+})
+
 module.exports = router;
